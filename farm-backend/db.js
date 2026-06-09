2ba20c4a69baf36edf79309f64ec395851394e54
yashpratapkushwaha966 <yashkushwaha485005@gmail.com>
2026-06-09 16:59:55 +0530

    Updated FarmConnect project

--- farm-backend/db.js
+++ farm-backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("MongoDB connected");
   } catch (error) {
     console.log("MongoDB error:", error.message);
